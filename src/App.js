b17b995e4cb8de4a32102cbb262a78f788f42644
erikhaandrikman <e.haandrikman@metrological.com>
2019-12-15 23:24:24 +0100

    init blueprint

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Lightning } from "wpe-lightning-sdk";
+
+export default class App extends Lightning.Component {
+
+    static getFonts() {
+        return [
+
+        ]
+    }
+
+    static _template() {
+        return {
+
+        }
+    }
+
+    _construct() {
+
+    }
+
+    _init() {
+
+    }
+}
